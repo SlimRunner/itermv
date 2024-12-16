d3c0debe920dae928612384282d055f2f5562099
SlimRunner <dv.polygon@gmail.com>
2024-12-16 02:05:57 -0800

    Re enable file operations

--- itermv/itermv/main.py
+++ itermv/itermv/main.py
@@ -13,14 +13,14 @@ def main():
     args = getArguments()
     included, ignored = getFileNames(args)
 
-    # printNameMapping(included, ignored, args)
+    printNameMapping(included, ignored, args)
 
-    # if len(included) > 0 and askUser(args):
-    #     if args.overlap:
-    #         schedule = renameFiles(included, args.dry_run)
-    #     else:
-    #         schedule = renameDisjointFiles(included, args.dry_run)
+    if len(included) > 0 and askUser(args):
+        if args.overlap:
+            schedule = renameFiles(included, args.dry_run)
+        else:
+            schedule = renameDisjointFiles(included, args.dry_run)
 
-    #     printChangesMade(schedule, args)
-    # elif len(included) == 0:
-    #     print("\nNo files are selected.")
+        printChangesMade(schedule, args)
+    elif len(included) == 0:
+        print("\nNo files are selected.")
